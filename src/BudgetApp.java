144e33af7edca996c267c9b91ac544a874bf2c97
night780 <96319709+night780@users.noreply.github.com>
2023-01-16 20:48:26 -0800

    Monthly Income

--- src/BudgetApp.java
+++ src/BudgetApp.java
@@ -14,11 +14,31 @@ public class BudgetApp {
         // Create a scanner to read input from the user
         Scanner input = new Scanner(System.in);
 
-        // Prompt the user to enter their income
+
+        // This is prompting the user to enter their monthly income.
         System.out.print("Enter your monthly income: ");
         double income = input.nextDouble();
         budget.put("Income", income);
 
 
+        // This is prompting the user to enter their monthly housing expenses.
+        System.out.print("Enter your monthly housing expenses: ");
+        double housing = input.nextDouble();
+        budget.put("Housing", housing);
+
+        // This is prompting the user to enter their monthly transportation expenses.
+        System.out.print("Enter your monthly transportation expenses: ");
+        double transportation = input.nextDouble();
+        budget.put("Transportation", transportation);
+
+        // This is prompting the user to enter their monthly food expenses.
+        System.out.print("Enter your monthly food expenses: ");
+        double food = input.nextDouble();
+        budget.put("Food", food);
+
+        // This is prompting the user to enter their monthly entertainment expenses.
+        System.out.print("Enter your monthly entertainment expenses: ");
+        double entertainment = input.nextDouble();
+        budget.put("Entertainment", entertainment);
     }
 }
