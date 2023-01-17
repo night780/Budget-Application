3b53f2d8a45fd052f30aaefeaca3dc129d752018
night780 <96319709+night780@users.noreply.github.com>
2023-01-16 20:43:36 -0800

    Monthly Income

--- src/BudgetApp.java
+++ src/BudgetApp.java
@@ -1,2 +1,24 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class BudgetApp {
+    public static void main(String[] args) {
+        HashMap<String, Double> budget = new HashMap<>();
+        budget.put("Income", 0.0);
+        budget.put("Housing", 0.0);
+        budget.put("Transportation", 0.0);
+        budget.put("Food", 0.0);
+        budget.put("Entertainment", 0.0);
+        budget.put("Savings", 0.0);
+
+        // Create a scanner to read input from the user
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter their income
+        System.out.print("Enter your monthly income: ");
+        double income = input.nextDouble();
+        budget.put("Income", income);
+
+
+    }
 }
